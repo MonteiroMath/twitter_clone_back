ec99f04266ca06c783c8cb3e03f9ea395226f0ea
Matheus <math.a.monteiro@gmail.com>
2022-04-07 11:31:30 -0300

    initial routes for user and tweet

--- routes/tweets.js
+++ routes/tweets.js
@@ -1,14 +1,11 @@
 const express = require("express");
 const router = express.Router();
+const { getUserTweets, getTweet } = require("../controllers/tweets");
 
 //get Tweets from a user
-router.get("/user/:id", (req, res) => {
-  return;
-});
+router.get("/user/:id", getUserTweets);
 
 //get a Tweet by id
-router.get("/:id", (req, res) => {
-    return;
-});
+router.get("/:id", getTweet);
 
 module.exports = router;
