95a5d89672d18da0266a554919df10c98697b746
Matheus <math.a.monteiro@gmail.com>
2022-04-08 10:40:48 -0300

    route skeletons + like route

--- controllers/tweets.js
+++ controllers/tweets.js
@@ -1,3 +1,5 @@
+const { default: tweets } = require("../../twitter/src/placeholders/tweets");
+
 const tweets_ph = [
   {
     id: 0,
@@ -145,7 +147,105 @@ function getTweet(req, res) {
   res.json(tweet);
 }
 
+function postTweet(req, res) {
+  /* get the new tweet content from the body of the requirement. As a placeholder, uses lenght as id.
+FUnction create NewTweet formats the new tweet. Return the newly registered tweet.
+*/
+
+  const { newTweet } = req.body;
+
+  if (!newTweet) {
+    return res.status(400).json({
+      success: false,
+      msg: "The content of the new tweet must be sent",
+    });
+  }
+
+  let tt_count = tweets_ph.length;
+
+  let tweet = createNewTweet(tt_count + 1, newTweet);
+
+  tweets_ph.push(tweet);
+
+  res.json({ success: true, tweet });
+}
+
+function answer(req, res) {
+  return;
+}
+
+function retweet(req, res) {
+  return;
+}
+
+function like(req, res) {
+  
+  const { id } = req.params;
+  const { userId } = req.body;
+
+  if (!userId) {
+    return res
+      .status(400)
+      .json({ sucess: false, msg: "The user id must be informed" });
+  }
+
+  let tweet = tweets_ph.find((tweet) => tweet.id === id);
+
+  if (!tweet) {
+    return res
+      .status(404)
+      .json({ success: false, msg: `Tweet ${id} not found.` });
+  }
+
+  if (tweet.liked_by.includes(userId)) {
+    return res
+      .status(400)
+      .json({ success: false, msg: `User ${userId} already liked this tweet` });
+  }
+
+  tweet.liked_by.push(userId);
+
+  res.json({ success: true, tweet });
+}
+
+//move to utility function
+
+function createNewTweet(id, content) {
+  /*
+
+    content: {
+      message,
+      attach,
+      poll,
+      retweet,
+      pollSettings
+    }
+
+  */
+  return {
+    id,
+    author: 1,
+    created: new Date().getTime(),
+    message: content.message,
+    attach: content.attach,
+    poll: content.poll,
+    retweet: content.retweet,
+    retweeted_by: [],
+    liked_by: [],
+    comment_ids: [],
+    pollSettings: {
+      choices: content.pollSettings.choices,
+      pollLen: content.pollSettings.pollLen,
+      votes: [0, 0],
+    },
+  };
+}
+
 module.exports = {
   getUserTweets,
   getTweet,
+  postTweet,
+  like,
+  answer,
+  retweet,
 };
--- routes/tweets.js
+++ routes/tweets.js
@@ -1,11 +1,33 @@
 const express = require("express");
+const { routes } = require("../app");
 const router = express.Router();
-const { getUserTweets, getTweet } = require("../controllers/tweets");
+const {
+  getUserTweets,
+  getTweet,
+  postTweet,
+  like,
+  answer,
+  retweet,
+} = require("../controllers/tweets");
+
+//Post a new tweet
+router.post("/", postTweet);
 
 //get Tweets from a user
 router.get("/user/:id", getUserTweets);
 
 //get a Tweet by id
 router.get("/:id", getTweet);
+
+//answer route
+
+router.post("/:id/answer", answer);
+
+//retweet route
+
+routes.post("/:id/retweet", retweet);
+
+//like route
+router.put("/:id/like", like);
 
 module.exports = router;
