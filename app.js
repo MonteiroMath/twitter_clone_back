afd6ff8123fb197a8492acd8fbeeaaf08280c74b
Matheus <math.a.monteiro@gmail.com>
2023-03-10 10:40:02 -0300

    enabled cors

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const express = require("express");
 const path = require("path");
 const cookieParser = require("cookie-parser");
 const logger = require("morgan");
+const cors = require("cors");
 require("dotenv").config();
 
 const User = require("./models/users");
@@ -12,6 +13,7 @@ const tweetsRouter = require("./routes/tweets");
 
 const app = express();
 
+app.use(cors());
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
