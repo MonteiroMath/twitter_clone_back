43135733687380f586156848b3add8efd8945c34
Matheus Monteiro <math.a.monteiro@gmail.com>
2024-07-16 10:07:56 -0300

    new add message function

--- controllers/messages.js
+++ controllers/messages.js
@@ -53,33 +53,29 @@ function postConversation(req, res) {
 }
 
 function postMessage(req, res, next) {
+  const { conversationID } = req.params;
   const { newMessage } = req.body;
 
   if (!newMessage) {
     throw new Error("Request must contain the new message data");
   }
 
-  const { message, authorID, recipientID } = newMessage;
+  const { message, userID } = newMessage;
 
-  if (!authorID || !recipientID || !message) {
+  if (!userID || !message) {
     throw new Error("Missing required data on newMessage");
   }
 
-  if (authorID === recipientID) {
-    throw new Error("User cannot message himself");
-  }
+  const findAuthorPromise = User.findByPk(userID);
+  const findConversationPromise = Conversation.findByPk(conversationID);
 
-  const findAuthorPromise = User.findByPk(authorID);
-  const findRecipientPromise = User.findByPk(recipientID);
+  Promise.all([findAuthorPromise, findConversationPromise])
+    .then(([author, conversation]) => {
+      if (!author) throw new Error(`User ${userID} not found`);
+      if (!conversation) throw new Error(`User ${conversationID} not found`);
 
-  Promise.all([findAuthorPromise, findRecipientPromise])
-    .then(([author, recipient]) => {
-      if (!author) throw new Error(`User ${authorID} not found`);
-      if (!recipient) throw new Error(`User ${recipientID} not found`);
-
-      return Message.create({
-        authorID,
-        recipientID,
+      return conversation.addMessage({
+        userID,
         message,
       });
     })
